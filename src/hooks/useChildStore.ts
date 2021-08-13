a41b836e8607cb88653da7e2311932481dab54fa
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2021-08-13 12:55:13 +0200

    Fix all ts issues

--- src/hooks/useChildStore.ts
+++ src/hooks/useChildStore.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Reducer } from 'redux';
+import { Reducer, Store } from 'redux';
 import { useStore } from 'react-redux';
 import { Saga } from '@redux-saga/types';
 import { InjectableStore } from '@modusbox/redux-utils/lib/injectors/types';
@@ -7,7 +7,7 @@ import { InjectableStore } from '@modusbox/redux-utils/lib/injectors/types';
 // Installs the reducer on the parent app and makes sure it is used
 export default function useChildStore(reducer: Reducer, saga: Saga) {
   const store = useStore() as InjectableStore;
-  const [childStore, setChildStore] = useState(null);
+  const [childStore, setChildStore] = useState<Store | null>(null);
 
   useEffect(() => {
     function injectAndGetStoreInstance() {
